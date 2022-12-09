67772b46bc7b5b2ea24645d5da4af2d78f0bd7b6
Cleber Moura <cleber.t.moura@gmail.com>
2022-12-09 18:11:28 -0300

    adding comments

--- src/app/pages/my-checkouts/my-checkouts.component.ts
+++ src/app/pages/my-checkouts/my-checkouts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { concatMap, map, tap, toArray } from 'rxjs/operators';
 import { IOrderItem } from 'src/app/models/order-item.model';
 import { IOrder } from 'src/app/models/order.model';
 import { OrderItemService } from 'src/app/services/order-item.service';
@@ -28,40 +28,40 @@ export class MyCheckoutsComponent implements OnInit {
     this.orderService
       .query()
       .pipe(
-        map(resOrders => (this.orders = resOrders ?? [])),
-        tap(orders => {
-          orders.forEach(order => {
-            this.loadOrderItems(order);
-          });
-        })
-      )
-      .subscribe(orders => {
-        this.orders = orders;
-      });
-  }
+        concatMap(resOrders => (this.orders = resOrders ?? [])),
+        tap(order => {
+          this.ordersCollapseMap[order.id] = { collapsed: true }
+        }),
+        // com o concatMap as requisições ao backend são realizadas de forma sequencial, e emitidas uma a uma
+        concatMap(order => this.loadOrderItems(order)),
 
-  loadOrderItems(order: IOrder): Subscription {
-    this.ordersCollapseMap[order.id] = { collapsed: true };
-    return this.orderItemService
-      .queryByOrder(order.id)
-      .pipe(
-        map(resOrderItems => (order.items = resOrderItems ?? [])),
-        tap(orderItems => {
-          orderItems.forEach(orderItem => {
-            this.loadOrderItemProduct(orderItem);
-          });
-        })
+        // com o mergeMap as requisições ao backend são realizadas de forma paralela
+        // mergeMap(order => this.loadOrderItems(order)),
+
+        // o switchMap cancela a subscrições de requisições anteriores que não tenham sido completadas
+        // switchMap(order => this.loadOrderItems(order)),
+
+        // converte as emissões em um array
+        toArray()
       )
       .subscribe();
   }
 
-  loadOrderItemProduct(orderItem: IOrderItem): Subscription {
-    return this.productService
-      .find(orderItem.product!.id)
-      .pipe(
-        tap(product => (orderItem.product = product ?? orderItem.product))
-      )
-      .subscribe();
+  loadOrderItems(order: IOrder): Observable<IOrderItem[]> {
+    return this.orderItemService.queryByOrder(order.id).pipe(
+      concatMap(resOrderItems => (order.items = resOrderItems ?? [])),
+      concatMap(orderItem => this.loadOrderItemProduct(orderItem)),
+      // mergeMap(orderItem => this.loadOrderItemProduct(orderItem)),
+      // switchMap(orderItem => this.loadOrderItemProduct(orderItem)),
+      toArray()
+    );
+  }
+
+  loadOrderItemProduct(orderItem: IOrderItem): Observable<IOrderItem> {
+    return this.productService.find(orderItem.product!.id).pipe(
+      tap(product => (orderItem.product = product ?? orderItem.product)),
+      map(() => orderItem)
+    );
   }
 
 }
